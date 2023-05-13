2d1508de644a89749c771bd0f85a00ded96e29fe
Curtis Aliwah <aliwahcurtis5@gmail.com>
2023-05-13 18:00:18 +0300

    Changes to service-section

--- js/script.js
+++ js/script.js
@@ -29,3 +29,47 @@ menu_item.forEach((item) => {
 window.onscroll = function () { myFunction() };
 
 /*Navbar menu*/
+
+
+// Services
+function updateServiceDescription1() {
+    let serviceDescription1 = document.getElementById("service-description-1");
+
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        serviceDescription1.textContent = "I design and develop websites tailored to your brand identity and business goals, creating a powerful online presence that drives results.";
+    } else {
+        serviceDescription1.textContent = "  I design and develop modern, user-friendly websites that help your business succeed online.My custom designs are tailored to your  unique brand identity and business goals.Let me help you create a powerful online presence that drives results for your business."
+    }
+}
+
+function updateServiceDescription2() {
+    let serviceDescription2 = document.getElementById("service-description-2");
+
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        serviceDescription2.textContent = "I design and develop websites tailored to your brand identity and business goals, creating a powerful online presence that drives results.";
+    } else {
+        serviceDescription2.textContent = "  I design and develop modern, user-friendly websites that help your business succeed online.My custom designs are tailored to your  unique brand identity and business goals.Let me help you create a powerful online presence that drives results for your business."
+    }
+}
+
+function updateServiceDescription3() {
+    let serviceDescription3 = document.getElementById("service-description-3");
+
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        serviceDescription3.textContent = "I design and develop websites tailored to your brand identity and business goals, creating a powerful online presence that drives results.";
+    } else {
+        serviceDescription3.textContent = "  I design and develop modern, user-friendly websites that help your business succeed online.My custom designs are tailored to your  unique brand identity and business goals.Let me help you create a powerful online presence that drives results for your business."
+    }
+}
+
+
+
+updateServiceDescription1();
+updateServiceDescription2();
+updateServiceDescription3();
+
+
+window.addEventListener("resize", updateServiceDescription1);
+window.addEventListener("resize", updateServiceDescription2);
+window.addEventListener("resize", updateServiceDescription3);
+// Services
